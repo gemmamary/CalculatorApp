be2d22e6e6b87569227c7e899f8ac268ea6c494d
gemmamary <gemmamarymcgee@gmail.com>
2020-08-26 20:46:31 +0100

    added tests for multiplying with negative and positive numbers

--- Calculator.Tests/CalculationTests.cs
+++ Calculator.Tests/CalculationTests.cs
@@ -7,16 +7,16 @@ namespace Calculator.Tests
     [TestClass]
     public class CalculationTests
     {
-        public int firstNumber = 10;
-        public int secondNumber = 2;
-        public int firstNegative = -10;
-        public int secondNegative = -2;
+        //public int firstNumber = 10;
+        //public int secondNumber = 2;
+        //public int firstNegative = -10;
+        //public int secondNegative = -2;
 
         [TestMethod]
         public void CanAddPositiveNumbers()
         { 
             var expectedSum = 12;
-            var sum = Calculation.AddNumbers(firstNumber, secondNumber);
+            var sum = Calculation.AddNumbers(8, 4);
             Assert.AreEqual(expectedSum, sum);
         }
 
@@ -24,72 +24,97 @@ namespace Calculator.Tests
         public void CanAddNegativeNumbers()
         { 
             var expectedSum = -12;
-            var sum = Calculation.AddNumbers(firstNegative, secondNegative);
+            var sum = Calculation.AddNumbers(-8, -4);
             Assert.AreEqual(expectedSum, sum);
         }
 
         [TestMethod]
         public void CanAddNegativeAndPositiveNumber()
         {
-            var expectedSum = 0;
-            var sum = Calculation.AddNumbers(secondNegative, secondNumber);
+            var expectedSum = -4;
+            var sum = Calculation.AddNumbers(-8, 4);
             Assert.AreEqual(expectedSum, sum);
         }
 
         [TestMethod]
         public void CanAddPositiveAndNegativeNumber()
         {
-            var expectedSum = -8;
-            var sum = Calculation.AddNumbers(secondNumber, firstNegative);
+            var expectedSum = 4;
+            var sum = Calculation.AddNumbers(8, -4);
             Assert.AreEqual(expectedSum, sum);
         }
 
         [TestMethod]
         public void CanSubtractNumbers()
         { 
-            var expectedSum = 8;
-            var sum = Calculation.SubtractNumbers(firstNumber, secondNumber);
+            var expectedSum = 4;
+            var sum = Calculation.SubtractNumbers(8, 4);
             Assert.AreEqual(expectedSum, sum);
         }
 
         [TestMethod]
         public void CanSubtractNegativeNumbers()
         {
-            var expectedSum = -8;
-            var sum = Calculation.SubtractNumbers(firstNegative, secondNegative);
+            var expectedSum = -4;
+            var sum = Calculation.SubtractNumbers(-8, -4);
             Assert.AreEqual(expectedSum, sum);
         }
 
         [TestMethod]
         public void CanSubtractNegativeAndPositiveNumber()
         {
-            var expectedSum = -4;
-            var sum = Calculation.SubtractNumbers(secondNegative, secondNumber);
+            var expectedSum = -12;
+            var sum = Calculation.SubtractNumbers(-8, 4);
             Assert.AreEqual(expectedSum, sum);
         }
 
         [TestMethod]
         public void CanSubtractPositiveAndNegativeNumber()
+
         {
             var expectedSum = 12;
-            var sum = Calculation.SubtractNumbers(secondNumber, firstNegative);
+            var sum = Calculation.SubtractNumbers(8, -4);
             Assert.AreEqual(expectedSum, sum);
         }
 
         [TestMethod]
         public void CanMultiplyNumbers()
         { 
-            var expectedSum = 20;
-            var sum = Calculation.MultiplyNumbers(firstNumber, secondNumber);
-            Assert.AreEqual(expectedSum, sum, "That sum is incorrect");
+            var expectedSum = 32;
+            var sum = Calculation.MultiplyNumbers(8, 4);
+            Assert.AreEqual(expectedSum, sum);
+        }
+
+        [TestMethod]
+        public void CanMultiplyNegativeNumbers()
+        {
+            var expectedSum = 32;
+            var sum = Calculation.MultiplyNumbers(-8, -4);
+            Assert.AreEqual(expectedSum, sum);
+        }
+
+        [TestMethod]
+        public void CanMultiplyNegativeAndPositiveNumber()
+        {
+            var expectedSum = -32;
+            var sum = Calculation.MultiplyNumbers(-8, 4);
+            Assert.AreEqual(expectedSum, sum);
+        }
+
+        [TestMethod]
+        public void CanMultiplyPositiveAndNegativeNumber()
+        {
+            var expectedSum = -32;
+            var sum = Calculation.MultiplyNumbers(8, -4);
+            Assert.AreEqual(expectedSum, sum);
         }
 
         [TestMethod]
         public void CanDivideNumbers()
         { 
             var expectedSum = 5;
-            var sum = Calculation.DivideNumbers(firstNumber, secondNumber);
-            Assert.AreEqual(expectedSum, sum, "That sum is incorrect");
+            var sum = Calculation.DivideNumbers(10, 2);
+            Assert.AreEqual(expectedSum, sum);
         }
 
     }
