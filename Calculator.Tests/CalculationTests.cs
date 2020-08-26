17644b3fae46c87ee324627e59fec23188615892
gemmamary <gemmamarymcgee@gmail.com>
2020-08-26 20:51:11 +0100

    added tests for dividing negative and positive numbers

--- Calculator.Tests/CalculationTests.cs
+++ Calculator.Tests/CalculationTests.cs
@@ -112,8 +112,32 @@ namespace Calculator.Tests
         [TestMethod]
         public void CanDivideNumbers()
         { 
-            var expectedSum = 5;
-            var sum = Calculation.DivideNumbers(10, 2);
+            var expectedSum = 2;
+            var sum = Calculation.DivideNumbers(8, 4);
+            Assert.AreEqual(expectedSum, sum);
+        }
+
+        [TestMethod]
+        public void CanDivideNegativeNumbers()
+        {
+            var expectedSum = 2;
+            var sum = Calculation.DivideNumbers(-8, -4);
+            Assert.AreEqual(expectedSum, sum);
+        }
+
+        [TestMethod]
+        public void CanDivideNegativeAndPositiveNumber()
+        {
+            var expectedSum = -2;
+            var sum = Calculation.DivideNumbers(-8, 4);
+            Assert.AreEqual(expectedSum, sum);
+        }
+
+        [TestMethod]
+        public void CanDividePositiveAndNegativeNumber()
+        {
+            var expectedSum = -2;
+            var sum = Calculation.DivideNumbers(8, -4);
             Assert.AreEqual(expectedSum, sum);
         }
 
